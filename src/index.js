9a4f02b33797421d1add1b42297d145ad87c8afb
samaksha <kumarsamaksha21@gmail.com>
2023-04-29 06:41:40 +0000

    updated index

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ connectToMongoDB(process.env.DB_URL).then(() =>
 const app = express();
 // config.update({region: region});
 // Init S3 client
-// console.log(process.env.AWS_REGION);
+console.log(process.env.AWS_ACCESS_KEY_ID);
 
 const s3 = new S3Client({credentials: {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -28,6 +28,7 @@ const s3 = new S3Client({credentials: {
 region: process.env.AWS_REGION});
 
 // Setup server middlewares
+// console.log(process.env);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -139,79 +140,87 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.post("/presigned", async (req, res) => {
-  // Prepare S3 command that will be executed
-  const params = {
-    Bucket: process.env.AWS_REGION,
+    // Prepare S3 command that will be executed
+    const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
     Key: v4(),
-  };
-  const command = new PutObjectCommand(params);
-  // console.log(s3);
-  // Generate presigned url (expiration in seconds)
-  const presignedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
-  const id = nanoid(10)
-  await URL.create({
-    shortId: id,
-    redirectURL: params.Key,
-  });
+    };
+    const command = new PutObjectCommand(params);
+    // console.log(s3);
+    // Generate presigned url (expiration in seconds)
+    try {
+        const presignedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
+        const id = nanoid(10)
+        await URL.create({
+        shortId: id,
+        redirectURL: params.Key,
+        });
 
-  res.json({
-    code: 200,
-    result: {presignedUrl: presignedUrl, shortId: id},
-  });
+        console.log(id,presignedUrl);
+        return res.json({
+        code: 200,
+        result: {presignedUrl: presignedUrl, shortId: id},
+        });
+    }
+    catch (err) {
+    console.log(err);
+    return res.json({code:400});
+    }
 });
 
 
 app.get("/upload/:id", async (req, res) => {
-  const shortId = req.params.id;
-  const entry = await URL.findOne(
+    const shortId = req.params.id;
+    const entry = await URL.findOne(
     {
-      shortId,
+        shortId,
     }
-  );
-  if(!entry) return res.json({code:400});
-  // console.log(entry.redirectURL);
-  var resURL = await getPresignedUrls(entry.redirectURL);
-  console.log(resURL);
-  if(!resURL || resURL.presignedUrls == 0) res.json({code:400}); 
-  res.redirect(resURL.presignedUrls[0]);
+    );
+    if(!entry) return res.json({code:400});
+    console.log(entry.redirectURL);
+    var resURL = await getPresignedUrls(entry.redirectURL);
+    console.log(resURL);
+    if(!resURL || resURL.presignedUrls.length == 0) return res.json({code:400}); 
+    console.log("upload :id");
+    return res.redirect(resURL.presignedUrls[0]);
 });
 
 const getKeys = async () => {
-  const command = new ListObjectsV2Command({
-    Bucket: process.env.AWS_REGION,
-  });
-  try {
-  const { Contents = [] } = await s3.send(command);
-  // const result = await s3.send(command);
-  // console.log(result);
-  // console.log(contents);
-  return Contents.map((files) => files.Key);
-  }
-  catch(err)
-  {
+    const command = new ListObjectsV2Command({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    });
+    try {
+    const { Contents = [] } = await s3.send(command);
+    // const result = await s3.send(command);
+    // console.log(result);
+    // console.log(contents);
+    return Contents.map((files) => files.Key);
+    }
+    catch(err)
+    {
     console.log(err);
     return [];
-  }
+    }
 }
 
 const getPresignedUrls = async(redirectURL) => {
-  try {
+    try {
     const fileKeys = await getKeys();
     const filteredFileKeys = fileKeys.filter((key) => {if(key == redirectURL) return key});
     console.log(filteredFileKeys);
 
     const presignedUrls = await Promise.all(filteredFileKeys.map((key) => {
-      const command = new GetObjectCommand({Bucket: process.env.AWS_REGION, Key: key});
-      const result =  getSignedUrl(s3, command, {expiresIn: 900});
-      return (result);
+        const command = new GetObjectCommand({Bucket: process.env.AWS_BUCKET_NAME, Key: key});
+        const result =  getSignedUrl(s3, command, {expiresIn: 900});
+        return (result);
     }));
 
     return { presignedUrls };
   }
-  catch(err) {
+    catch(err) {
     console.log(err);
     return {err};
-  }
+    }
 };
 
 
