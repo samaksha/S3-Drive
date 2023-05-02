f13356542179ffa262acad668f50485a13ecbe90
samaksha <kumarsamaksha21@gmail.com>
2023-05-02 16:10:57 +0530

    update

--- src/index.js
+++ src/index.js
@@ -46,6 +46,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
+  response.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
