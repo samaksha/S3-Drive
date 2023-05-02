4733b54518e2f4e352b1fa0cb023052a76c381f8
samaksha <kumarsamaksha21@gmail.com>
2023-05-02 16:03:44 +0530

    update

--- src/index.js
+++ src/index.js
@@ -41,10 +41,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://s3-drive-git-master-kumarsamaksha21.vercel.app"
   );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
